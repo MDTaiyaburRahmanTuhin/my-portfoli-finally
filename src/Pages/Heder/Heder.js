b71d9b37c67bfcdbed03d469327d4dba4678f609
MDTaiyaburRahmanTuhin <mdtaiyaburrahmantuhin99>
2022-01-06 15:48:51 +0600

    some edit

--- src/Pages/Heder/Heder.js
+++ src/Pages/Heder/Heder.js
@@ -17,7 +17,7 @@ const Heder = () => {
                     </div>
                     <p>A self-motivated and enthusiastic web developer with a deep interest in JavaScript. To work in the Software industry with modern web technologies of different local & multinational Software/ IT agencies of Bangladesh and grow rapidly with increasing responsibilities.</p>
                     <Button className='me-2'>About Me</Button>
-                    <Link to={{ pathname: "https://drive.google.com/file/d/1GahNC1VGKcyN3D8AfTF8_Bz7OU0heArI/view?usp=sharing" }} target="_blank" ><Button>My Resume</Button></Link>
+                    <Link to={{ pathname: "https://drive.google.com/file/d/1IIJtMRF_ttF0XMxXLpBsHkqzb_j2fh-o/view?usp=sharing" }} target="_blank" ><Button>My Resume</Button></Link>
                 </div>
                 <div className="col-lg-6 col-md-12">
                     <img loading='lazy' src="https://i.ibb.co/vws82kL/Tuhin.jpg" alt="" className='image float-end' />
